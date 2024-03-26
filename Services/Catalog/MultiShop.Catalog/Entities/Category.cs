8b6c1cc6f1caa2bb4dfce72f082ac70ba4afe2cc
Burak Sekmen <burakskmn07@gmail.com>
2024-03-27 00:21:40 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId]// Mango Db bu id olduğunu anlaması için
+        [BsonRepresentation(BsonType.ObjectId)] // id'nin ObjectId olduğunu belirtmek için
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
