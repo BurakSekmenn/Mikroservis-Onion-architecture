36fae441a937357993bbc70055d409b0045bfb15
Burak Sekmen <burakskmn07@gmail.com>
2024-03-27 02:20:00 +0300

    Controller Eklendi, Deneme Test işlemleri yapıldıç

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductImageController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Services.ProductImageServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImageController : ControllerBase
+    {
+        private readonly IProductImageService _ProductImageService;
+
+        public ProductImageController(IProductImageService ProductImageService)
+        {
+            _ProductImageService = ProductImageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductImageList()
+        {
+            var categories = await _ProductImageService.GetResultProductImageAsync();
+            return Ok(categories);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductImageById(string id)
+        {
+            var ProductImage = await _ProductImageService.GetByIdProductImageAsync(id);
+            return Ok(ProductImage);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
+        {
+            await _ProductImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok("Ürün Resmi Başarıyla Eklendi.");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductImage(string id)
+        {
+            await _ProductImageService.DeleteProductImageAsync(id);
+            return Ok("Ürün Resmi Başarıyla Silindi.");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
+        {
+            await _ProductImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok();
+        }
+    }
+}
