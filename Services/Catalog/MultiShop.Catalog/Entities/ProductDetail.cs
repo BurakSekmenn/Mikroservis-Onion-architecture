27d1e01aea03b9eacc562aac58f266e326e7b336
Burak Sekmen <burakskmn07@gmail.com>
2024-03-27 00:58:13 +0300

    Dto eklemesi yapıldı

--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -1,10 +1,20 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MultiShop.Catalog.Entities
 {
     public class ProductDetail
     {
+
+        [BsonId]// Mango Db bu id olduğunu anlaması için
+        [BsonRepresentation(BsonType.ObjectId)] // id'nin ObjectId olduğunu belirtmek için
         public string ProductDetailId { get; set; }
         public string ProductDescription { get; set; }
         public string ProductInfo { get; set; }
+
+        public string ProductId { get; set; }
+        [BsonIgnore]//MongoDb'ye kaydetme demek
+        public Product Product { get; set; }    
 
     }
 }
--- Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -1,14 +1,19 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MultiShop.Catalog.Entities
 {
     public class ProductImage
     {
+        [BsonId]// Mango Db bu id olduğunu anlaması için
+        [BsonRepresentation(BsonType.ObjectId)] // id'nin ObjectId olduğunu belirtmek için
         public string ProductImageId { get; set; }
         public string Image1 { get; set; }
         public string Image2 { get; set; }
         public string Image3 { get; set; }
 
         public string ProductId { get; set; }
-
+        [BsonIgnore]//MongoDb'ye kaydetme demek
         public Product Product { get; set; }
     }
 }
