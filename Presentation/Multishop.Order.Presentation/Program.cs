f8b212b3b7181d6fd56324c07bce01c05e2857e6
Burak Sekmen <burakskmn07@gmail.com>
2024-04-01 18:43:17 +0300

    Program cs düzenlendi

--- Presentation/Multishop.Order.Presentation/Program.cs
+++ Presentation/Multishop.Order.Presentation/Program.cs
@@ -1,8 +1,16 @@
 using Multishop.Order.Application.Features.CQRS.Handlers.AddresHandlers;
 using Multishop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using Multishop.Order.Application.Interfaces;
+using Multishop.Order.Application.Services;
+using Multishop.Order.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+
+builder.Services.AddApplicationService(builder.Configuration);
+
+#region
 builder.Services.AddScoped<GetAddressQueryHandler>();
 builder.Services.AddScoped<GetAddressByIdQueryHandler>();
 builder.Services.AddScoped<CreateAddressCommandHandler>();
@@ -14,6 +22,8 @@ builder.Services.AddScoped<GetOrderDetailQueryHandler>();
 builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
 builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
 builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
+#endregion
+
 
 
 builder.Services.AddControllers();
